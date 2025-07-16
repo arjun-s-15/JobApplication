72cf8aaec37069a45a07c60df015a34a8ec03a27
arjun-s-15 <iamarjun9003@gmail.com>
2025-07-16 11:26:07 +0530

    added actuator to project

--- JobApplication/src/main/java/arjunSingh/JobApplication/JobApplication.java
+++ JobApplication/src/main/java/arjunSingh/JobApplication/JobApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JobApplication {
-
+// 6:16
 	public static void main(String[] args) {
 		SpringApplication.run(JobApplication.class, args);
 	}
+
 }
